8cb488c0b825ac1c837f63a16f6b115e5a11e542
moiz.akhtar <moiz.akhtar@pitb.gov.pk>
2023-06-16 02:40:12 +0500

    #work_order_changes

--- resources/js/Components/CustomLink.vue
+++ resources/js/Components/CustomLink.vue
@@ -5,10 +5,21 @@ defineProps({
         default: 'blue',
     },
 });
+
+const linkClass = (type) => {
+    if (type == "yellow") {
+        return "inline-flex items-center px-4 py-2 bg-yellow-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-yellow-500 focus:bg-yellow-500 active:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 transition ease-in-out duration-150";
+    }
+
+    if (type == "purple") {
+        return "inline-flex items-center px-4 py-2 bg-purple-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-purple-500 focus:bg-purple-500 active:bg-purple-500 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 transition ease-in-out duration-150";
+    }
+};
+
 </script>
 
 <template>
-    <a :class="'inline-flex items-center px-4 py-2 bg-' + type + '-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-' + type + '-700 focus:bg-' + type + '-700 active:bg-' + type + '-900 focus:outline-none focus:ring-2 focus:ring-' + type + '-500 focus:ring-offset-2 transition ease-in-out duration-150'">
+    <a :class="linkClass(type)">
         <slot />
     </a>
 </template>
